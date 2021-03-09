b112c06edfec37aa6f072205ba9fe67bb056a126
Eugene <iyao@mail.ru>
2021-03-09 11:51:35 +0300

    first commit

---
+++ app/src/main/java/ru/samsung/itacademy/mdev/task4164/MainActivity.kt
@@ -0,0 +1,21 @@
+package ru.samsung.itacademy.mdev.task4164
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonStart.setOnClickListener(View.OnClickListener {
+            ForegroundService.startService(this, "Foreground Service is running...")
+        })
+        buttonStop.setOnClickListener(View.OnClickListener {
+            ForegroundService.stopService(this)
+        })
+    }
+
+}
